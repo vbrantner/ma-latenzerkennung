bdbebabf0a64c2bd46c748d4f2ec1f46fa71a680
Vinzenz Brantner <vbrantner@gmail.com>
2023-12-05 21:12:20 +0100

    update

--- decode_single_image.py
+++ decode_single_image.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from pyzbar.pyzbar import decode
 
-with Image.open("./split_images/split_16_1.png") as img:
+with Image.open("./frame003699.png") as img:
     results = decode(img)
     print(results)
